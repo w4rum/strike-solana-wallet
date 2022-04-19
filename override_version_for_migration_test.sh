58005b7a659a0bb14ff79393c593b15848cfa899
Benjamin Holzman <bholzman@gmail.com>
2022-04-19 11:12:18 -0400

    fix for build (#69)
    Co-authored-by: Ben Holzman <bholzman@strikeprotocols.com>

--- override_version_for_migration_test.sh
+++ override_version_for_migration_test.sh
@@ -1,4 +1,4 @@
-VERSION=$(make version)
+VERSION=$(make -s version)
 
 perl -pi -e "s/static MIGRATION_TEST_VERSION: u32 = VERSION;/static MIGRATION_TEST_VERSION: u32 = ${VERSION};/" src/handlers/migrate_handler.rs
 perl -pi -e 's/pub static VERSION: u32 = .*;/pub static VERSION: u32 = 0;/' src/version.rs
